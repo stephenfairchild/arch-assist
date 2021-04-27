4f999213537bfba87564aff07d9a22986ab9cbcb
Stephen Fairchild <stephen.fairchild@researchsquare.com>
2021-04-27 14:15:10 -0400

    update package script

--- 03-packages.sh
+++ 03-packages.sh
@@ -18,7 +18,7 @@ fi
 # Install system packages
 pacman -S --noconfirm --needed neovim git zsh wget tmux sed openssl openssh notification-daemon jq fzf dunst
 
-# X
+# GUI + window server
 pacman -S --noconfirm --needed xorg-server xorg-xinit
 
 # Install graphics card driver
@@ -26,7 +26,7 @@ if $(lspci | grep "VGA.*Intel" >/dev/null 2>&1); then
   pacman -S --noconfirm libva-intel-driver xf86-video-intel
 fi
 
-# Desktop environment
+# Desktop + Window environment
 pacman -S --noconfirm --needed \
 i3-gaps i3status i3lock \
 dmenu feh \
@@ -35,29 +35,39 @@ powerline powerline-common powerline-fonts
 
 # Install package-query, flux-gui, polybar?
 # Install yay?
-# Install fonts?
+
+# Fonts
+pacman -S --noconfirm --needed ttf-fira-code
 
 # Browser
 pacman -S --noconfirm --needed chromium
 #pacman -S --noconfirm --needed brave
 
-# Install media tools
+# Media
 pacman -S --noconfirm --needed pulseaudio pulseaudio-alsa pulseaudio-bluetooth \
 blueman bluez bluez-utils \
 alsa-utils
 
-# Install programming languages
+# Languages
 pacman -S --noconfirm --needed ruby rust nodejs python3 python2 perl go
 # nvm??
 
-# Install dev tools
+# Development
 pacman -S --noconfirm --needed terminator tmate mariadb aws-cli diff-so-fancy
 ## saw??
 
-# Install productivity tools
-#yay gyazo unzip thunar the_silver_searcher
+# Docker
+pacman -S --noconfirm --needed docker docker-compose
+systemctl start docker
+systemctl enable docker
 
-# Install Gaming tool
+# TODO we shouldn't hardcode the user...
+usermod -a -G docker stephen
+
+# Productivity
+#yay slack gyazo unzip thunar the_silver_searcher
+
+# Gaming
 #pacman -S --noconfirm --needed playonlinux minecraft-launcher
 
 # Install docker
