4f18c7224fbd65620346dee42ca43be29215a8ab
Stephen Fairchild <stephen.fairchild@researchsquare.com>
2021-04-27 10:04:46 -0400

    set zsh as shell by default

--- 04-provision.sh
+++ 04-provision.sh
@@ -1,5 +1,12 @@
 #! /usr/bin/sh
 
-curl -o- https://raw.githubusercontent.com/stephenfairchild/Dotfiles/master/install.sh | bash
+# Clone dotfiles
+#curl -o- https://raw.githubusercontent.com/stephenfairchild/Dotfiles/master/install.sh | bash
 
+# Setup Git
+git config --global user.email "stephen.fairchild@researchsquare.com"
+git config --global user.name "Stephen Fairchild"
+
+# Set default shell to zsh
+sudo chsh -s /bin/zsh $(whoami)
 
