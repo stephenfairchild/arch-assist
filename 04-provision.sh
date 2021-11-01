778491d0440d004124935d903eb01982dc7bcb93
Stephen Fairchild <stephen.fairchild@researchsquare.com>
2021-11-01 10:47:17 -0400

    some cleanup and sensible updates

--- 04-provision.sh
+++ 04-provision.sh
@@ -1,5 +1,9 @@
 #! /usr/bin/sh
 
+# TODO add a check to make sure the user is not root.
+# TODO create /home/{user}/.local/share if it doesnt exist.
+# TODO If not logged into last pass prompt to login.
+
 # Set default shell to zsh
 sudo chsh -s /bin/zsh $(whoami)
 
@@ -26,21 +30,15 @@ lpass show ssh-config --notes --fixed-strings | tee ~/.ssh/config > /dev/null
 ## Set Permissions
 chmod 600 ~/.ssh/id_rsa
 chmod 644 ~/.ssh/id_rsa.pub
-#
+
 ## Create .vim.local
 lpass show vim-local --notes --fixed-strings | tee ~/.vim.local > /dev/null
-
-## Configure AWS credentials
-aws configure
-
-## ECR Login
-eval $(aws ecr get-login --no-include-email)
 
 ## Clone Documents
 cd ~
 git clone git@github.com:stephenfairchild/documents.git
 
-## Clone Dotfiles
+## Install Dotfiles
 curl -o- https://raw.githubusercontent.com/stephenfairchild/Dotfiles/master/install.sh | bash
 
 ## Setup Git
@@ -50,3 +48,7 @@ git config --global user.name "Stephen Fairchild"
 # Copy in script files
 chown stephen:stephen /usr/local/bin && cd /usr/local/bin
 git clone git@github.com:stephenfairchild/usr-local-bin.git
+
+## Configure AWS credentials
+aws configure
+eval $(aws ecr get-login --no-include-email)
