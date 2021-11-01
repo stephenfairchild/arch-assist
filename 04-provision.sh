dda933da25734eecfa259293506b55f5039f96a8
Stephen Fairchild <stephen.fairchild@researchsquare.com>
2021-11-01 11:15:04 -0400

    cleanup and add some todos

--- 04-provision.sh
+++ 04-provision.sh
@@ -1,8 +1,15 @@
 #! /usr/bin/sh
 
+# This file uses the lastpass CLI to copy secrets onto a new machine. Secrets
+# in this context are things like AWS keys, SSH private/public keypairs and
+# the vim.local. All of this assumes that the lastpass secrets are in place.
+#
+# It also handles the AWS login.
+
 # TODO add a check to make sure the user is not root.
 # TODO create /home/{user}/.local/share if it doesnt exist.
 # TODO If not logged into last pass prompt to login.
+# TODO Warn about things that will be overwritten, AWS, SSH, vim.local
 
 # Set default shell to zsh
 sudo chsh -s /bin/zsh $(whoami)
@@ -20,19 +27,20 @@ touch ~/.ssh/id_rsa
 touch ~/.ssh/id_rsa.pub
 touch ~/.ssh/config
 
-## Create SSH key files
+## Create the AWS secrets placeholder
+mkdir -m700 -p ~/.aws
+touch ~/.aws/credentials
+
+## Create new system files based on the lastpass files
 lpass show ssh-private-key --notes --fixed-strings | tee ~/.ssh/id_rsa > /dev/null
 lpass show ssh-public-key --notes --fixed-strings | tee ~/.ssh/id_rsa.pub > /dev/null
-
-## Create SSH config file
 lpass show ssh-config --notes --fixed-strings | tee ~/.ssh/config > /dev/null
+lpass show rs-aws-keys --notes --fixed-strings | tee ~/.aws/credentials > /dev/null
+lpass show vim-local --notes --fixed-strings | tee ~/.vim.local > /dev/null
 
 ## Set Permissions
 chmod 600 ~/.ssh/id_rsa
 chmod 644 ~/.ssh/id_rsa.pub
-
-## Create .vim.local
-lpass show vim-local --notes --fixed-strings | tee ~/.vim.local > /dev/null
 
 ## Clone Documents
 cd ~
@@ -48,6 +56,7 @@ git config --global user.name "Stephen Fairchild"
 # Copy in script files
 chown stephen:stephen /usr/local/bin && cd /usr/local/bin
 git clone git@github.com:stephenfairchild/usr-local-bin.git
+
 
 ## Configure AWS credentials
 aws configure
