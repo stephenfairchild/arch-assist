6b23e2cc3ed82c96246b077c0c560592d51c7553
Stephen Fairchild <stephen.fairchild@researchsquare.com>
2021-11-01 10:16:55 -0400

    uncomment some important items

--- 03-packages.sh
+++ 03-packages.sh
@@ -42,12 +42,6 @@ pacman -S --noconfirm --needed ruby rust nodejs python3 python2 perl go php
 
 # Code completion support in Vim
 pacman -S --noconfirm --needed python-neovim
-# Link composer so it can be ran globally for vim completion
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-# cd into /phpactor vim plugins and run `composer install`
 
 # Development
 pacman -S --noconfirm --needed terminator tmate mariadb aws-cli diff-so-fancy prettier the_silver_searcher npm android-studio
@@ -60,4 +54,3 @@ usermod -a -G docker stephen
 
 # Productivity
 pacman -S --noconfirm --needed lastpass-cli htop netcat
-#yay slack gyazo unzip playonlinux minecraft-launcher saw nvm brave package-query flux-gui polybar
--- 04-provision.sh
+++ 04-provision.sh
@@ -1,52 +1,51 @@
 #! /usr/bin/sh
 
 # Set default shell to zsh
-#sudo chsh -s /bin/zsh $(whoami)
-#
+sudo chsh -s /bin/zsh $(whoami)
+
 ## Remove bash defaults
-#rm -rvf ~/.bashrc ~/.bash_history
-#
+rm -rvf ~/.bashrc ~/.bash_history
+
 ## Remove existing SSH and .local if they exist
-#rm -rvf ~/.ssh
-#rm ~/.vim.local
-#
-## Setup
-#mkdir -m700 -p ~/.ssh
-#touch ~/.ssh/id_rsa
-#touch ~/.ssh/id_rsa.pub
-#touch ~/.ssh/config
-#
+rm -rvf ~/.ssh
+rm ~/.vim.local
+
+## Setup new SSH files
+mkdir -m700 -p ~/.ssh
+touch ~/.ssh/id_rsa
+touch ~/.ssh/id_rsa.pub
+touch ~/.ssh/config
+
 ## Create SSH key files
-#lpass show ssh-private-key --notes --fixed-strings | tee ~/.ssh/id_rsa > /dev/null
-#lpass show ssh-public-key --notes --fixed-strings | tee ~/.ssh/id_rsa.pub > /dev/null
-#
+lpass show ssh-private-key --notes --fixed-strings | tee ~/.ssh/id_rsa > /dev/null
+lpass show ssh-public-key --notes --fixed-strings | tee ~/.ssh/id_rsa.pub > /dev/null
+
 ## Create SSH config file
-#lpass show ssh-config --notes --fixed-strings | tee ~/.ssh/config > /dev/null
-#
+lpass show ssh-config --notes --fixed-strings | tee ~/.ssh/config > /dev/null
+
 ## Set Permissions
-#chmod 600 ~/.ssh/id_rsa
-#chmod 644 ~/.ssh/id_rsa.pub
+chmod 600 ~/.ssh/id_rsa
+chmod 644 ~/.ssh/id_rsa.pub
 #
 ## Create .vim.local
-#lpass show vim-local --notes --fixed-strings | tee ~/.vim.local > /dev/null
-#
+lpass show vim-local --notes --fixed-strings | tee ~/.vim.local > /dev/null
+
 ## Configure AWS credentials
-#aws configure
-#
+aws configure
+
 ## ECR Login
-#eval $(aws ecr get-login --no-include-email)
-#
-#cd ~
-#
+eval $(aws ecr get-login --no-include-email)
+
 ## Clone Documents
-#git clone git@github.com:stephenfairchild/documents.git
-#
+cd ~
+git clone git@github.com:stephenfairchild/documents.git
+
 ## Clone Dotfiles
-#curl -o- https://raw.githubusercontent.com/stephenfairchild/Dotfiles/master/install.sh | bash
-#
+curl -o- https://raw.githubusercontent.com/stephenfairchild/Dotfiles/master/install.sh | bash
+
 ## Setup Git
-#git config --global user.email "stephen.fairchild@researchsquare.com"
-#git config --global user.name "Stephen Fairchild"
+git config --global user.email "stephen.fairchild@researchsquare.com"
+git config --global user.name "Stephen Fairchild"
 
 # Copy in script files
 chown stephen:stephen /usr/local/bin && cd /usr/local/bin
